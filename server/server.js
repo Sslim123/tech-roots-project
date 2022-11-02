e2aabf1fe479912627ae823c9b8f33fda9204dca
Abd@94 <45979612+Abdalrazaq94@users.noreply.github.com>
2022-11-02 19:48:18 +0000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ import { connectDb, disconnectDb } from "./db";
 import config from "./utils/config";
 import logger from "./utils/logger";
 
-const port = parseInt(process.env.PORT || "3000");
 
 const server = http.createServer(app);
 
