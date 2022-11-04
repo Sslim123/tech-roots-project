2535bb5398bb0eafbed3d17f7fee8f857f08ab47
Sslim123 <slimsmasm5@gmail.com>
2022-11-04 23:25:57 +0000

    add create laptop request
    - LaptopForm in the client
    - POST /api/laptop_request endpoint in the server
    
    before running this code, create the laptop_request table in the cyf db with the following command
    
    create table laptop_request (id serial primary key, firstname varchar(100) ,lastname varchar(100), email varchar(100), phonenumber varchar(100));

--- server/api.js
+++ server/api.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import db from "./db";
 
 import logger from "./utils/logger";
 
@@ -8,5 +9,19 @@ router.get("/", (_, res) => {
 	logger.debug("Welcoming everyone...");
 	res.json({ message: "Hello, world!" });
 });
+router.post("/laptop_request", (req, res) => {
+	let firstOne = req.body.firstName;
+	let secondOne = req.body.secondName;
+	let emailOne = req.body.email;
+	let phonOne = req.body.phoneNumber;
+	const query =
+		" insert into laptop_request (firstname, lastname, email, phonenumber) values ($1, $2, $3, $4)";
+	db.query(query, [firstOne, secondOne, emailOne, phonOne])
+		.then(() => res.send("result.rows"))
+		.catch((error) => {
+			console.error(error);
+			res.status(400).json({ success: " was not   success" });
+		});
+}); //console.lo
 
 export default router;
