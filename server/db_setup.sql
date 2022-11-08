7c8f1a0453762c5b0e0634f82b7e1e78c0cf93bd
“Fareedh <2617283y@student.gla.ac.uk>
2022-11-08 02:32:43 +0000

    created laptop_donations table

--- server/db_setup.sql
+++ server/db_setup.sql
@@ -1,9 +1,19 @@
-DROP Table IF EXISTS laptop_request;
+DROP Table IF EXISTS laptop_request, laptop_donations;
 create table laptop_request (
     id serial primary key,
     firstname varchar(100),
     lastname varchar(100),
     email varchar(100),
     phonenumber varchar(100)
+);
+
+create table laptop_donations (
+    id serial primary key,
+    name varchar(100),
+    address varchar(100),
+    laptops INT,
+    phonenumber varchar(100),
+    email varchar(100),
+    deliveryOption varchar(25)
 );
 
