029b7aa1b6b7ed281b3f36e02749e01a50f8989b
“Fareedh <2617283y@student.gla.ac.uk>
2022-11-09 20:25:47 +0000

    added sample data for laptop donation table

--- server/db_populate.sql
+++ server/db_populate.sql
@@ -1,2 +1,14 @@
 insert into laptop_request (firstname, lastname, email, phonenumber)
-values ('Salem', 'Daifan', 'salem@cyf.com', '0748484877')
+values ('Salem', 'Daifan', 'salem@cyf.com', '0748484877');
+
+insert into laptop_donation (name, address, number_of_laptops, phone_number, email, delivery_option) 
+values ('Bob','G151SU', 2, '073837837780', 'bob@gmail.com', 'PICK UP');
+
+insert into laptop_donation (name, address, number_of_laptops, phone_number, email, delivery_option) 
+values ('Paul','G151AA', 2, '07383712380', 'paul@gmail.com', 'DROP OFF');
+
+insert into laptop_donation (name, address, number_of_laptops, phone_number, email, delivery_option) 
+values ('James','G251SB', 2, '07335237830', 'james@gmail.com', 'PICK UP');
+
+insert into laptop_donation (name, address, number_of_laptops, phone_number, email, delivery_option) 
+values ('Ivan','G101SJ', 2, '07363457820', 'ivan@gmail.com', 'SHIP');
