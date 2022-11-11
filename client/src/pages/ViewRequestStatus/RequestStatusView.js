1af061b280c73d4cf442d276262201cf319c8489
“Fareedh <2617283y@student.gla.ac.uk>
2022-11-11 22:32:45 +0000

    requeststatusview

--- client/src/pages/ViewRequestStatus/RequestStatusView.js
+++ client/src/pages/ViewRequestStatus/RequestStatusView.js
@@ -14,47 +14,45 @@ const RequestStatusView = () => {
 			.then((data) => setStatus(data));
 	}, []);
 
-	// console.log(status);
-
-	// if (status !== null) {
-	// 	if (status === "WAITING") {
-	// 		return (
-	// 			<>
-	// 				<div>
-	// 					Thank you for your laptop request. You are on the waiting list. We
-	// 					will send you an notification when this changes
-	// 				</div>
-	// 				<button>Cancel my request</button>
-	// 			</>
-	// 		);
-	// 	} else if (status === "ASSIGNED") {
-	// 		return (
-	// 			<>
-	// 				You have been assigned a laptop. Please confirm your address can be
-	// 				shared so it can be sent to you."
-	// 			</>
-	// 		);
-	// 	} else if (status === "ACCEPTED") {
-	// 		return (
-	// 			<>
-	// 				Please let us know when you have received your laptop / when you have
-	// 				picked up your laptop at address.
-	// 			</>
-	// 		);
-	// 	} else if (status === "CANCELLED") {
-	// 		return <>"This request has been cancelled, as requested."</>;
-	// 	} else if (status === "FULLFILLED") {
-	// 		return (
-	// 			<>
-	// 				Sweet! You now have your laptop. Time to start working on your
-	// 				application to Code Your Future?
-	// 			</>
-	// 		);
-	// 	}
-	// } else {
-	// 	return <>Oops, something went wrong.../ this request doesn't exist</>;
-	// }
-	return <div>RequestStatusView</div>;
+	if (status !== null) {
+		if (status === "WAITING") {
+			return (
+				<>
+					<div>
+						Thank you for your laptop request. You are on the waiting list. We
+						will send you an notification when this changes
+					</div>
+					<button>Cancel my request</button>
+				</>
+			);
+		} else if (status === "ASSIGNED") {
+			return (
+				<>
+					You have been assigned a laptop. Please confirm your address can be
+					shared so it can be sent to you."
+				</>
+			);
+		} else if (status === "ACCEPTED") {
+			return (
+				<>
+					Please let us know when you have received your laptop / when you have
+					picked up your laptop at address.
+				</>
+			);
+		} else if (status === "CANCELLED") {
+			return <>"This request has been cancelled, as requested."</>;
+		} else if (status === "FULLFILLED") {
+			return (
+				<>
+					Sweet! You now have your laptop. Time to start working on your
+					application to Code Your Future?
+				</>
+			);
+		}
+	} else {
+		return <>Oops, something went wrong.../ this request doesn't exist</>;
+	}
+	
 };
 
 export default RequestStatusView
