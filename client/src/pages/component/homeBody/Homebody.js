6c589481bb80636c24d2b68ab31f7045f30253ae
Abd@94 <45979612+Abdalrazaq94@users.noreply.github.com>
2022-11-15 20:50:42 +0000

    Feature/created home path pages

---
+++ client/src/pages/component/homeBody/Homebody.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Homebody.css'
+
+function Homebody() {
+	return (
+		<>
+			<div className="homebody">
+				
+				<p>Home body</p>
+			</div>
+		</>
+	);
+}
+
+export default Homebody;
