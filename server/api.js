9cb4064d2d800effa71476d77d08340094d6c1ec
“Fareedh <2617283y@student.gla.ac.uk>
2022-11-15 18:25:08 +0000

    navigation to laptop-request-status/:id

--- server/api.js
+++ server/api.js
@@ -140,27 +140,28 @@ router.post("/laptop_request", (req, res) => {
 	let email = req.body.email;
 	let phoneNumber = req.body.phoneNumber;
 	const query =
-		" insert into laptop_request (firstname, lastname, email, phonenumber) values ($1, $2, $3, $4)";
+		" insert into laptop_request (firstname, lastname, email, phonenumber) values ($1, $2, $3, $4) returning id";
 	db.query(query, [firstName, lastName, email, phoneNumber])
-		.then(() => res.send("result.rows"))
+		.then((queryResult) => res.send(queryResult.rows[0]))
 		.catch((error) => {
 			console.error(error);
-			res.status(400).json({ success: " was not   success" });
+			res.status(400).json({ success: " was not success" });
 		});
 }); //console.lo
 
 router.get("/laptop_request", async (req, res) => {
 	try {
 		const result = await db.query("SELECT * from laptop_request");
-
 		const laptopRequests = result.rows.map((row) => {
 			return {
+
 				firstName: row.firstname,
 				lastName: row.lastname,
 				email: row.email,
 				phoneNumber: row.phonenumber,
 			};
 		});
+
 		res.json(laptopRequests);
 	} catch (e) {
 		console.error(e);
