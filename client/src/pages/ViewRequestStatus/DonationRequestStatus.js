ff39a081d01b70eb39a0514bf372eaf597792e4b
Abd@94 <45979612+Abdalrazaq94@users.noreply.github.com>
2022-11-17 13:25:19 +0000

    delete navigate from node & delete unused useeffecte

--- client/src/pages/ViewRequestStatus/DonationRequestStatus.js
+++ client/src/pages/ViewRequestStatus/DonationRequestStatus.js
@@ -1,21 +1,14 @@
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import './StatusView.css'
-import { useParams } from "react-router-dom";
+
 
 
 function DonationRequestStatues() {
-    const [request, setRequest] = useState(null);
-	// this helps get the id from the router
-	const { id } = useParams();
 
-    //  this use effect/ it may be not the right Api that we are going to Fetch the data with
-    // this only template 
-	useEffect(() => {
-		fetch(`/laptop_assignment/{id}`)
-			.then((res) => res.json())
-			.then((data) => setRequest(data));
-	}, [id]);
+	// this helps get the id from the router
+	
+
     return ( <>
     
     <div className='main-container-donation-status'>
