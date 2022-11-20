f10005771ac0afc943b7a8a4fc8f737c6f169e44
Abd@94 <45979612+Abdalrazaq94@users.noreply.github.com>
2022-11-20 23:20:00 +0000

    mission page & fix homebody height

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -3,13 +3,16 @@ import Navbar from "../component/navbar/navbar";
 import Homebody from "../component/homeBody/Homebody";
 import Footer from "../component/footer/Footer";
 import "./Home.css";
+import Mission from "../component/Mission/Mission";
 
 export function Home() {
 	return (
 		<>
-			< Navbar />
-			< Homebody />
-			< Footer />
+			<Navbar />
+			<Homebody />
+
+			<Mission />
+			<Footer />
 		</>
 	);
 }
