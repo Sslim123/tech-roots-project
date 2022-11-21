1102a47cd21f0b72d5a37443efd8ca57b6b67d53
Abd@94 <45979612+Abdalrazaq94@users.noreply.github.com>
2022-11-21 16:31:29 +0000

    update <p> to <h1>  and delete <>

--- client/src/pages/component/Mission/Mission.js
+++ client/src/pages/component/Mission/Mission.js
@@ -3,21 +3,19 @@ import "./Mission.css";
 
 function Mission() {
 	return (
-		<>
-			<div className="mission-constrainer">
-				<div className="mission-card">
-					<p className="mission-title">Our Mission</p>
-					<p className="mission-text">
-						Laptop loop is an opportunity for people seeking asylum to receive
-						digital support in the form of laptops. We aim to connect people
-						with donors and givers and, ensure that donated laptops reach those
-						who need them. We understand many asylum seekers experience a lot of
-						barriers and hardships. So, if you have an extra laptop - we have
-						those who need it. Donate a laptop. Thank you.
-					</p>
-				</div>
+		<div className="mission-constrainer">
+			<div className="mission-card">
+				<p className="mission-title">Our Mission</p>
+				<p className="mission-text">
+					Laptop loop is an opportunity for people seeking asylum to receive
+					digital support in the form of laptops. We aim to connect people with
+					donors and givers and, ensure that donated laptops reach those who
+					need them. We understand many asylum seekers experience a lot of
+					barriers and hardships. So, if you have an extra laptop - we have
+					those who need it. Donate a laptop. Thank you.
+				</p>
 			</div>
-		</>
+		</div>
 	);
 }
 
--- client/src/pages/component/footer/Footer.js
+++ client/src/pages/component/footer/Footer.js
@@ -2,13 +2,11 @@ import React from "react";
 import "./Footer.css";
 function Footer() {
 	return (
-		<>
-			<div className="footer-container">
-				<div className="footer-card">
-					<div className="footer-logo"></div>
-				</div>
+		<div className="footer-container">
+			<div className="footer-card">
+				<div className="footer-logo"></div>
 			</div>
-		</>
+		</div>
 	);
 }
 
