87a2fdf15ff622d308ac3abdfbf2f6d9617cdfb2
“Fareedh <2617283y@student.gla.ac.uk>
2022-11-26 00:14:36 +0000

    testing with no populate data

--- server/db_populate.sql
+++ server/db_populate.sql
@@ -1,29 +1,29 @@
---  laptop request data
+-- --  laptop request data
 
- insert into laptop_request (firstname, lastname, email, phonenumber, uuid)
- values ('Salem', 'Daifan', 'salem@cyf.com', '0748484877', 'fake-uuid-1');
+--  insert into laptop_request (firstname, lastname, email, phonenumber, uuid)
+--  values ('Salem', 'Daifan', 'salem@cyf.com', '0748484877', 'fake-uuid-1');
 
- insert into laptop_request (firstname, lastname, email, phonenumber, uuid)
- values ('Fareedh', 'Yawe', 'yawe@cyf.com', '0748484877', 'fake-uuid-2');
+--  insert into laptop_request (firstname, lastname, email, phonenumber, uuid)
+--  values ('Fareedh', 'Yawe', 'yawe@cyf.com', '0748484877', 'fake-uuid-2');
 
- insert into laptop_request (firstname, lastname, email, phonenumber, uuid)
- values ('Abed', 'Abed', 'abed@cyf.com', '0748484877', 'fake-uuid-3');
+--  insert into laptop_request (firstname, lastname, email, phonenumber, uuid)
+--  values ('Abed', 'Abed', 'abed@cyf.com', '0748484877', 'fake-uuid-3');
 
- insert into laptop_request (firstname, lastname, email, phonenumber, uuid)
- values ('Tandy', 'Nicole', 'tandy@cyf.com', '0748484877', 'fake-uuid-4');
+--  insert into laptop_request (firstname, lastname, email, phonenumber, uuid)
+--  values ('Tandy', 'Nicole', 'tandy@cyf.com', '0748484877', 'fake-uuid-4');
 
 
 
- -- laptop donation data
+--  -- laptop donation data
 
---  insert into laptop_donation (name, address, number_of_laptops, phone_number, email, delivery_option,uuid) 
---  values ('Bob','G151SU', 1, '073837837780', 'bob@gmail.com', 'PICKUP', 'fake-uuid-5');
+-- --  insert into laptop_donation (name, address, number_of_laptops, phone_number, email, delivery_option,uuid) 
+-- --  values ('Bob','G151SU', 1, '073837837780', 'bob@gmail.com', 'PICKUP', 'fake-uuid-5');
 
---  insert into laptop_donation (name, address, number_of_laptops, phone_number, email, delivery_option,uuid) 
---  values ('Paul','G151AA', 1, '07383712380', 'paul@gmail.com', 'SHIP', 'fake-uuid-6');
+-- --  insert into laptop_donation (name, address, number_of_laptops, phone_number, email, delivery_option,uuid) 
+-- --  values ('Paul','G151AA', 1, '07383712380', 'paul@gmail.com', 'SHIP', 'fake-uuid-6');
 
---  insert into laptop_donation (name, address, number_of_laptops, phone_number, email, delivery_option,uuid) 
---  values ('James','G251SB', 1, '07335237830', 'james@gmail.com', 'PICKUP', 'fake-uuid-7');
+-- --  insert into laptop_donation (name, address, number_of_laptops, phone_number, email, delivery_option,uuid) 
+-- --  values ('James','G251SB', 1, '07335237830', 'james@gmail.com', 'PICKUP', 'fake-uuid-7');
 
---  insert into laptop_donation (name, address, number_of_laptops, phone_number, email, delivery_option,uuid) 
---  values ('Ivan','G101SJ', 1, '07363457820', 'ivan@gmail.com', 'SHIP', 'fake-uuid-8');
+-- --  insert into laptop_donation (name, address, number_of_laptops, phone_number, email, delivery_option,uuid) 
+-- --  values ('Ivan','G101SJ', 1, '07363457820', 'ivan@gmail.com', 'SHIP', 'fake-uuid-8');
